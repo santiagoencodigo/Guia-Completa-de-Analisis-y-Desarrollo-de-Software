da06f54b15025fdcea068e1b4f42899532fd02f4
Santiago Muñeton <159667940+santiagoencodigo@users.noreply.github.com>
2025-11-14 08:03:45 -0500

    Trimestre# 3: Add Modulo_Datos.py for student data processing

---
+++ Trimestre #3/Programming/Modulo_Datos.py
@@ -0,0 +1,18 @@
+from Modulo_Ejecicios_y_Datos import datos
+
+nombre, ficha, nota1, nota2 = datos()
+
+print(f"nombre {nombre}")
+print(f"ficha {ficha}")
+print(f"nota1 {nota1}")
+print(f"nota2 {nota2}")
+
+promedio = nota1 + nota2
+
+print()
+print("Su promedio es", promedio)
+
+if promedio < 5:
+    print(f"No aprobo su nota es: {promedio}, vuelve a intentarlo...")
+else:
+    print(f"Felicitaciones, aprobo y su nota es: {promedio}")
