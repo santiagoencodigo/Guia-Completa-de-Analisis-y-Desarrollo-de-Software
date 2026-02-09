19948ab3f9dda6b64a119bf2692e50fa76aab134
Santiago Muñeton <159667940+santiagoencodigo@users.noreply.github.com>
2026-02-09 11:05:11 -0500

    code: Trimestre #4/02-construir-bases-de-datos: 2.2.2 - Creación Carpeta SQL, Base_de_Datos.sql - Ejercicio #1 de BD

---
+++ Trimestre #4/02-construir-bases-de-datos/sql/Base_de_Datos.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 09-02-2026 a las 13:59:21
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Base_de_Datos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `DOCUMENTO` int(11) NOT NULL,
+  `NOMBRE_COMPLETO` varchar(45) DEFAULT NULL,
+  `CORREO` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `ID_PRODUCTO` int(11) NOT NULL,
+  `NOMBRE_PRODUCTO` varchar(55) NOT NULL,
+  `PRECIO_PRODUCTO` int(11) NOT NULL,
+  `CANTIDAD` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venpro`
+--
+
+CREATE TABLE `venpro` (
+  `COPIA_PRODUCTO` int(11) NOT NULL,
+  `COPIA_VENTA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `ID_VENTA` int(11) NOT NULL,
+  `FECHA` date NOT NULL,
+  `PRECIO` int(11) NOT NULL,
+  `COPIA_DOCUMENTO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`DOCUMENTO`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`ID_PRODUCTO`),
+  ADD UNIQUE KEY `ID_PRODUCTO` (`ID_PRODUCTO`);
+
+--
+-- Indices de la tabla `venpro`
+--
+ALTER TABLE `venpro`
+  ADD KEY `COPIA_PRODUCTO` (`COPIA_PRODUCTO`),
+  ADD KEY `COPIA_VENTA` (`COPIA_VENTA`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`ID_VENTA`),
+  ADD KEY `COPIA_DOCUMENTO` (`COPIA_DOCUMENTO`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `ID_VENTA` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `venpro`
+--
+ALTER TABLE `venpro`
+  ADD CONSTRAINT `venpro_ibfk_1` FOREIGN KEY (`COPIA_PRODUCTO`) REFERENCES `producto` (`ID_PRODUCTO`),
+  ADD CONSTRAINT `venpro_ibfk_2` FOREIGN KEY (`COPIA_VENTA`) REFERENCES `venta` (`ID_VENTA`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`COPIA_DOCUMENTO`) REFERENCES `cliente` (`DOCUMENTO`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
